8c74667c6e1cbfea50530e0d844e9c6271fc6395
shahriar021 <shahriarc19@gmail.com>
2023-08-12 05:29:12 +0600

    now solve BMI

--- BMI/script.js
+++ BMI/script.js
@@ -1,36 +1,24 @@
-window.onload = () => {
-  let button = document.querySelector("#btn");
 
-  // Function for calculating BMI
-  button.addEventListener("click", calculateBMI);
-};
 
-function calculateBMI() {
-  /* Getting input from user into height variable.
-	Input is string so typecasting is necessary. */
-  let height = parseInt(document.querySelector("#height").value);
+var add = document.querySelector("#calculate");
 
-  /* Getting input from user into weight variable.
-	Input is string so typecasting is necessary.*/
-  let weight = parseInt(document.querySelector("#weight").value);
+add.addEventListener("click", function() {
+let h = parseFloat(document.querySelector("#height").value);
+console.log(h);
 
-  let result = document.querySelector("#result");
+let w = parseFloat(document.querySelector("#weight").value);
+console.log(w);
 
-  // Checking the user providing a proper
-  // value or not
-  if (height === "" || isNaN(height))
-    result.innerHTML = "Provide a valid Height!";
-  else if (weight === "" || isNaN(weight))
-    result.innerHTML = "Provide a valid Weight!";
-  // If both input is valid, calculate the bmi
-  else {
-    // Fixing upto 2 decimal places
-    let bmi = (weight / ((height * height) / 10000)).toFixed(2);
+  let bmi = w/(h*h);
+  document.querySelector("#result").innerHTML = bmi;
+  console.log(typeof(bmi));
+  if(isNaN(bmi)){
 
-    // Dividing as per the bmi conditions
-    if (bmi < 18.6) result.innerHTML = `Under Weight : <span>${bmi}</span>`;
-    else if (bmi >= 18.6 && bmi < 24.9)
-      result.innerHTML = `Normal : <span>${bmi}</span>`;
-    else result.innerHTML = `Over Weight : <span>${bmi}</span>`;
+    bmi=0;
+
+
+    document.querySelector("#result").innerHTML = "Please enter valid values";
   }
-}
+
+
+});
